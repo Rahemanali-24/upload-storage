9318cdbf6138ec4684806855af4913620405f3c6
Rahemanali-24 <159017790+Rahemanali-24@users.noreply.github.com>
2024-04-25 19:18:31 +0530

    ui part

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { UploadFileComponent } from './upload-file/upload-file.component';
 import { GalleryComponent } from './gallery/gallery.component';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   redirectTo: '/upload',
-  //   pathMatch: 'full',
-  // },
-  { path: '', component: UploadFileComponent },
+  {
+    path: '',
+    redirectTo: '/upload',
+    pathMatch: 'full',
+  },
+  { path: 'upload', component: UploadFileComponent },
   { path: 'gallery', component: GalleryComponent },
 ];
 
